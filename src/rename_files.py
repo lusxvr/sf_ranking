57a298e93a4717be1d536a04a1af04705ec0b6ae
Luis <luis.wiedmann@gmail.com>
2024-12-04 15:09:21 +0100

    data update

---
+++ src/rename_files.py
@@ -0,0 +1,15 @@
+import os
+
+def rename_files(folder_path):
+    files = os.listdir(folder_path)
+    for index, filename in enumerate(files):
+        file_extension = os.path.splitext(filename)[1]
+        new_name = f"{index:03d}{file_extension}"
+        old_file = os.path.join(folder_path, filename)
+        new_file = os.path.join(folder_path, new_name)
+        os.rename(old_file, new_file)
+        print(f"Renamed '{filename}' to '{new_name}'")
+
+if __name__ == "__main__":
+    folder_path = 'data'
+    rename_files(folder_path)
