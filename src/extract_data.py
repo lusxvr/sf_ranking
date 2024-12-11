b001d90ac7fdbc6eb9d55e1cebc4ffd7ad1fbd28
Luis <luis.wiedmann@gmail.com>
2024-12-11 14:58:53 +0100

    extraction from db

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,23 +1,24 @@
 import sqlite3
 import json
 
-# Connect to the SQLite database
-connection = sqlite3.connect("votes.db")
-cursor = connection.cursor()
+def extract_votes(db_path, json_path):
+    # Connect to the SQLite database
+    connection = sqlite3.connect("votes.db")
+    cursor = connection.cursor()
 
-# Query the numbers you need
-cursor.execute("SELECT * FROM votes")
-numbers = cursor.fetchall()  # Fetch all rows
+    # Query the numbers you need
+    cursor.execute("SELECT * FROM votes")
+    numbers = cursor.fetchall()  # Fetch all rows
 
-# Convert to a list of numbers
-votes = {}
-for row in numbers:
-    votes[row[0]] = row[1]
-#numbers_list = [row[0] for row in numbers]
+    # Convert to a list of numbers
+    votes = {}
+    for row in numbers:
+        votes[row[0]] = row[1]
+    #numbers_list = [row[0] for row in numbers]
 
-# Save to JSON
-with open("votes.json", "w") as json_file:
-    json.dump(votes, json_file)
+    # Save to JSON
+    with open("votes.json", "w") as json_file:
+        json.dump(votes, json_file)
 
-# Close the connection
-connection.close()
+    # Close the connection
+    connection.close()
